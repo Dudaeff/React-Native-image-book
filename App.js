01a963f2efd6f604a4d97421647f597c07c60305
Roma <r.dutkevich@gmail.com>
2023-05-17 22:33:02 +0300

    made auth screens

---
+++ App.js
@@ -0,0 +1,22 @@
+import { useFonts } from "expo-font";
+import { RegistrationScreen } from "./Screens/RegistrationScreen";
+import { LoginScreen } from "./Screens/LoginScreen";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
+    </>
+  );
+}
